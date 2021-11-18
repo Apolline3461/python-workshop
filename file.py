90cd0b0f7932e434cf815e77f02b0c7248b282a3
apolline <apolline.fontaine@epitech.eu>
2021-11-18 15:50:27 +0400

    exercice 1.21

--- file.py
+++ file.py
@@ -1,8 +1,15 @@
-print("Success")
+# print("Success")
 
-fruit = "banana"
-quantity = 3
-pie_crust = "empty"
-isOvenOn = False
+# fruit = "apple"
+# quantity = 5
+# pie_crust = "empty"
+# isOvenOn = False
 
-print(f"You have {quantity} {fruit} and the pie crust is {pie_crust}, is the oven on ? {isOvenOn}")
+def prepMyFruit(quantity, fruit, pie_crust):
+    print(f"You put {quantity} {fruit} on the crust")
+    pie_crust = f"filled with delicious {fruit}"
+    return pie_crust
+
+pie_crust = prepMyFruit(3, "apple", "empty")
+print("my pie is", pie_crust)
+# print(f"You have {quantity} {fruit} and the pie crust is {pie_crust}, is the oven on ? {isOvenOn}")
