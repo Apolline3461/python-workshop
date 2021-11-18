99b13395689338dd17714935fbffe74f752f5432
apolline <apolline.fontaine@epitech.eu>
2021-11-18 15:22:48 +0400

    add f flag

--- file.py
+++ file.py
@@ -1,1 +1,8 @@
 print("Success")
+
+fruit = "banana"
+quantity = 3
+pie_crust = "empty"
+isOvenOn = False
+
+print(f"You have {quantity} {fruit} and the pie crust is {pie_crust}, is the oven on ? {isOvenOn}")
