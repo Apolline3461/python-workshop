7d01f76e016ad9e29e7a49285b92146dcc8c1281
apolline <apolline.fontaine@epitech.eu>
2021-11-18 15:58:28 +0400

    exercise 1.2.2

--- file.py
+++ file.py
@@ -1,15 +1,18 @@
-# print("Success")
 
-# fruit = "apple"
-# quantity = 5
-# pie_crust = "empty"
-# isOvenOn = False
+is_oven_on = False
 
 def prepMyFruit(quantity, fruit, pie_crust):
     print(f"You put {quantity} {fruit} on the crust")
     pie_crust = f"filled with delicious {fruit}"
     return pie_crust
 
-pie_crust = prepMyFruit(3, "apple", "empty")
+def turn_on_oven(is_oven_on):
+    is_oven_on = "True"
+    return is_oven_on
+
+pie_crust = prepMyFruit(3, "apples", "empty")
+is_oven_on = turn_on_oven(is_oven_on)
 print("my pie is", pie_crust)
+print("Is the oven turned on ?", is_oven_on)
+
 # print(f"You have {quantity} {fruit} and the pie crust is {pie_crust}, is the oven on ? {isOvenOn}")
